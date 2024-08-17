d9728473932e4ebdd6665efbf4f59189f102bcdc
Bastiaan Stroosnijder <bas@strootje.com>
2024-08-17 15:31:23 +0200

    build + end image

--- src/app.tsx
+++ src/app.tsx
@@ -3,19 +3,18 @@ import "virtual:uno.css";
 
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
+import { I18nProvider } from "./contexts/I18nContext";
 import { PlausibleProvider } from "./contexts/PlausibleContext";
 import { SentryRouter } from "./contexts/SentryContext";
 
 export default function App() {
-	return (
-		<PlausibleProvider
-			apiHost="https://stats.strooweb.nl"
-			domain="strootje.com"
-			trackLocalhost={false}
-		>
-			<SentryRouter root={(props) => <Suspense>{props.children}</Suspense>}>
-				<FileRoutes />
-			</SentryRouter>
-		</PlausibleProvider>
-	);
+  return (
+    <I18nProvider locale="en">
+      <PlausibleProvider apiHost="https://stats.strooweb.nl" domain="strootje.com" trackLocalhost={false}>
+        <SentryRouter root={(props) => <Suspense>{props.children}</Suspense>}>
+          <FileRoutes />
+        </SentryRouter>
+      </PlausibleProvider>
+    </I18nProvider>
+  );
 }
